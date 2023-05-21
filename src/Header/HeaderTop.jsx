95bb4b6691d9ed3be382ab6bc065e5cb7107560e
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-05-22 02:02:12 +0330

    create HeaderTop file

---
+++ src/Header/HeaderTop.jsx
@@ -0,0 +1,44 @@
+import "./HeaderTop.css";
+
+function HeaderTop() {
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <div className="header-top">
+            <div className="app-logo">
+              <span className="app-logo__text">S</span>
+            </div>
+            <nav className="header-nav">
+              <ul className="header-nav__list">
+                <li className="header-nav__item">
+                  <a href="#" className="header-nav__link header-nav__link--active">
+                    Home
+                  </a>
+                </li>
+                <li className="header-nav__item">
+                  <a href="#" className="header-nav__link">
+                    About me
+                  </a>
+                </li>
+                <li className="header-nav__item">
+                  <a href="#" className="header-nav__link">
+                    Project
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="header-search">
+              <img src="/public/assets/svg/search.svg" alt="" />
+            </div>
+            <div className="hamburger">
+              <span className="hamburger-line"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeaderTop;
