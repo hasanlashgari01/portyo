65da1006754b26dffa5d0305c0de7f475bd6117e
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-05-22 17:44:39 +0330

    finish SERVICES section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Header/Header";
 import Company from "./components/Company";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Header />
       <Company />
+      <Services />
     </>
   );
 }
