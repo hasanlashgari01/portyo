e93e7aa9168ee8c82bdb35d7ec662b69a45591af
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-05-22 12:52:59 +0330

    finsih HEADER MAIN section

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,16 @@
 import HeaderTop from "./HeaderTop";
 import "./Header.css";
+import HeaderMain from "./HeaderMain";
 
 function Header() {
   return (
     <>
-      <HeaderTop />
+      <div className="header">
+        <div className="container">
+          <HeaderTop />
+          <HeaderMain />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/Header/HeaderMain.jsx
@@ -0,0 +1,44 @@
+import "./HeaderMain.css";
+
+function HeaderMain() {
+  return (
+    <>
+      <div className="header-main">
+        <div className="header-left">
+          <h5 className="header-left__suptitle">Hi, I am samantha</h5>
+          <h1 className="header-left__title">
+            Proffesional <span className="header-left__title-underline">product designer</span> based in USA{" "}
+          </h1>
+          <p className="header-left__description">
+            Varius amet, integer tellus non eget viverra. Ultrices tellus donec gravida id sed senectus dolor cursus sed. Ullamcorper tellus
+            ac cras nec, pretium laoreet duis.
+          </p>
+
+          <div className="header-left__links">
+            <a href="#" className="header-left__link">
+              Contact Us
+            </a>
+            <div className="header-left__icons">
+              <a href="#" className="header-left__icon">
+                <img src="/public/assets/svg/INSTAGRAM.svg" alt="" />
+              </a>
+              <a href="#" className="header-left__icon">
+                <img src="/public/assets/svg/DRIBBBLE.svg" alt="" />
+              </a>
+              <a href="#" className="header-left__icon">
+                <img src="/public/assets/svg/BEHANCE.svg" alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="header-right">
+          <div className="header-right__wrapper">
+            <img className="header-right__small" src="/public/assets/images/samantha-small.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeaderMain;
